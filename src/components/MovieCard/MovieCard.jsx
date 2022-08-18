ac6b7af1ae869fed0788086740649700dfb1f87f
Seo <seola.inf@gmail.com>
2022-08-18 14:41:54 +0700

    commit

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,12 +1,17 @@
-import React from "react";
 import PropTypes from "prop-types";
+import React from "react";
+import { MdOutlineStarPurple500, MdPlayArrow } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 import { apiConfig } from "../../api/apiConfig";
 import Button from "../Button/Button";
-import { MdOutlineStarPurple500, MdPlayArrow } from "react-icons/md";
 import "./movieCard.scss";
-import { Link } from "react-router-dom";
 
 const MovieCard = ({ data, cate }) => {
+  const navigate = useNavigate();
+  const handlePlay = () => {
+    navigate(`/${data?.media_type || cate}/${data.id}`);
+  };
+
   return (
     <div
       className="movieCard"
@@ -14,21 +19,19 @@ const MovieCard = ({ data, cate }) => {
       data-aos-duration="1000"
       data-aos-easing="ease-in-out"
     >
-      <Link to={`/${data?.media_type || cate}/${data.id}`}>
-        <div className="movieCard__img">
-          <img
-            src={apiConfig.w500Image(data.poster_path || data.backdrop_path)}
-            alt={data.original_title}
-          />
-        </div>
-      </Link>
+      <div className="movieCard__img">
+        <img
+          src={apiConfig.w500Image(data.poster_path || data.backdrop_path)}
+          alt={data.original_title}
+        />
+      </div>
       <div className="movieCard__info">
         <h4>{data.title || data.original_title || data.name}</h4>
         <span>
           {data.release_date?.split("-")[0] ||
             data.first_air_date?.split("-")[0]}
         </span>
-        <Button>
+        <Button onClick={handlePlay}>
           <MdPlayArrow className="icon" />
         </Button>
       </div>
